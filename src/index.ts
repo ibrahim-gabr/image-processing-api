338eacab7f035edac74686166779117a2ba27580
Ibrahim Abdallah <ibrahim.G@salla.sa>
2024-06-27 12:03:29 +0300

    add ejs and frontend to display images

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,15 @@ const PORT = envs.PORT;
 const app = express();
 
 app.use(envs.API_PREFIX, routes);
+
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+app.get('/', (req, res) => {
+	res.render('pages/home');
+});
+
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
 	console.log(`server started on port ${PORT}`);
-	console.log('hello world');
 });
