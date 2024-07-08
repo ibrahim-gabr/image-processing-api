93599a144f29da31a24e2556dc1b1876a04be9f4
Ibrahim Abdallah <ibrahim.G@salla.sa>
2024-07-08 12:12:11 +0300

    add unit tests

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -4,18 +4,23 @@ import fs, { promises as fspromises } from 'fs';
 
 const images = express.Router();
 
-images.get('/', (async (req, res) => {
-	const filename = req.query.filename as string;
-	const width = req.query.width as unknown as number;
-	const height = req.query.height as unknown as number;
-	['width', 'height', 'filename'].forEach((key: string) => {
-		if (!req.query[key] || req.query[key] === '') {
-			throw new Error(`${key} is required`);
-		}
-	});
-	const result = await resizeImage(filename, width, height);
-	console.log({ result });
-	res.type('jpeg').send(result);
+images.get('/', (async (req, res, next) => {
+	try {
+		const filename = req.query.filename as string;
+		const width = req.query.width as unknown as number;
+		const height = req.query.height as unknown as number;
+		['width', 'height', 'filename'].forEach((key: string) => {
+			if (!req.query[key] || req.query[key] === '') {
+				res.status(400).send(`${key} is required`);
+				next(new Error(`${key} is required`)); // Stop further execution
+			}
+		});
+		const result = await resizeImage(filename, width, height);
+		console.log({ result });
+		res.type('jpeg').send(result);
+	} catch (error) {
+		next(error);
+	}
 }) as RequestHandler);
 
 images.get('/list', async (req, res) => {
--- src/tests/imagesSpec.ts
+++ src/tests/imagesSpec.ts
@@ -5,7 +5,21 @@ import { HttpCode } from '../core';
 describe('images processing endpoint', () => {
 	it('does not get 404', async () => {
 		const res = await request(app).get('/api/v1/images');
-		const status = res.statusCode;
+		const status = res.status;
 		expect(status).not.toEqual(HttpCode.NOT_FOUND);
+	});
+	it('returns 400 if no query params', async () => {
+		const res = await request(app).get('/api/v1/images');
+		const status = res.status;
+		expect(status).toEqual(HttpCode.BAD_REQUEST);
+	});
+	it('returns if filename is not availble in images list', async () => {
+		const res = await request(app).get('/api/v1/images?filename=2.jpg&width=100&height=100');
+		const status = res.status;
+		expect(status).toEqual(HttpCode.INTERNAL_SERVER_ERROR);
+	});
+	it('returns buffer if query params are provided', async () => {
+		const res = await request(app).get('/api/v1/images?filename=encenadaport&width=100&height=100');
+		expect(res.body).toBeInstanceOf(Buffer);
 	});
 });
