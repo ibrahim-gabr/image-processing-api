0fb2eb3a720c544e78289dbd5b958a4d19995de7
ibrahim gabr <ibrahim.g@salla.sa>
2024-07-10 13:36:06 +0300

    fix with code review

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,10 @@ import imagesRoutes from './api/images';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
+routes.get('/', (req: express.Request, res: express.Response): void => {
 	res.send('main route');
 });
 
 routes.use('/images', imagesRoutes);
+
 export default routes;
